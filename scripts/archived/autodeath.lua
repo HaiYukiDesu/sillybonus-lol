aa01b3f8c232a3696ad8804f4818612a63d27cc1
bonus <donutorbonus@gmail.com>
2025-04-23 14:57:19 +0900

    Delete scripts directory

--- scripts/archived/autodeath.lua
+++
@@ -1,8 +0,0 @@
-repeat wait(1)
-local args = {
-    [1] = true
-}
-
-game:GetService("ReplicatedStorage"):WaitForChild("RemotesFolder"):WaitForChild("Underwater"):FireServer(unpack(args))
-until game.Players.LocalPlayer.Character.Humanoid.Health == 0
-game:GetService("ReplicatedStorage"):WaitForChild("RemotesFolder"):WaitForChild("PlayAgain"):FireServer()
