f67f34a426ddfa169e9ab39002896ceb0928d9d9
bonus <donutorbonus@gmail.com>
2025-06-28 14:16:44 +0900

    Create data.js

---
+++ growagardenleak/data.js
@@ -0,0 +1,679 @@
+    const allRawData = `
+        "Rafflesia": [
+        SeedName = "Rafflesia Seed";
+        SeedRarity = "Legendary";
+        StockChance = 10;
+        StockAmount = {1, 3};
+        Price = 3200;
+        PurchaseID = 3317729900;
+        GiftPurchaseID = 3317750415;
+        DisplayInShop = true;
+        LayoutOrder = 110;
+        Asset = "https://create.roblox.com/store/asset/83445943993020";
+        FruitIcon = "https://create.roblox.com/store/asset/78497832370658";
+        GrowthTime = 1;
+        FallbackPrice = 215;
+        ],
+
+        "Pitcher Plant": [
+        SeedName = "Pitcher Plant";
+        SeedRarity = "Divine";
+        StockChance = 220;
+        StockAmount = {1, 1};
+        Price = 7500000;
+        PurchaseID = 3317730202;
+        GiftPurchaseID = 3317750224;
+        DisplayInShop = true;
+        LayoutOrder = 250;
+        Asset = "https://create.roblox.com/store/asset/73483249581245";
+        FruitIcon = "https://create.roblox.com/store/asset/124700197819795";
+        GrowthTime = 1;
+        FallbackPrice = 715;
+        ],
+
+        "Burning Bud": [
+        SeedName = "Burning Bud";
+        SeedRarity = "Common";
+        StockChance = 0;
+        StockAmount = {0, 0};
+        Price = 50000000000000000;
+        PurchaseID = 3316826714;
+        GiftPurchaseID = 3316827012;
+        DisplayInShop = false;
+        LayoutOrder = 15;
+        Asset = "https://create.roblox.com/store/asset/73285874238558";
+        FruitIcon = "https://create.roblox.com/store/asset/129249941454691";
+        GrowthTime = 1;
+        FallbackPrice = 915;
+        ],
+
+        "Delphinium": [
+        SeedName = "Delphinium Seed";
+        SeedRarity = "Rare";
+        StockChance = 0;
+        StockAmount = {0, 0};
+        Price = 100000000000000000;
+        PurchaseID = 0;
+        DisplayInShop = false;
+        LayoutOrder = 350;
+        Asset = "https://create.roblox.com/store/asset/111328001776683";
+        FruitIcon = "https://create.roblox.com/store/asset/133880936305421";
+        GrowthTime = 1;
+        ],
+        
+        "Lily of the Valley": [
+        SeedName = "Lily of the Valley Seed";
+        SeedRarity = "Mythical";
+        StockChance = 0;
+        StockAmount = {0, 0};
+        Price = 100000000000000000;
+        PurchaseID = 0;
+        DisplayInShop = false;
+        LayoutOrder = 370;
+        Asset = "https://create.roblox.com/store/asset/108051617568505";
+        FruitIcon = "https://create.roblox.com/store/asset/119818724793701";
+        GrowthTime = 1;
+        ],
+
+        "Traveler's Fruit": [
+        SeedName = "Traveler's Fruit";
+        SeedRarity = "Divine";
+        StockChance = 0;
+        StockAmount = {0, 0};
+        Price = 100000000000000000;
+        PurchaseID = 0;
+        DisplayInShop = false;
+        LayoutOrder = 910;
+        Asset = "https://create.roblox.com/store/asset/104586947353350";
+        FruitIcon = "https://create.roblox.com/store/asset/84278912103459";
+        GrowthTime = 1;
+        ],
+        
+        "Peace Lily": [
+        SeedName = "Peace Lily Seed";
+		SeedRarity = "Rare";
+		StockChance = 0;
+		StockAmount = {0, 0};
+		Price = 100000000000000000;
+		PurchaseID = 0;
+		DisplayInShop = false;
+		LayoutOrder = 350;
+		Asset = "https://create.roblox.com/store/asset/133756602812253";
+		FruitIcon = "https://create.roblox.com/store/asset/111160733962340";
+		GrowthTime = 1;
+        ],
+        
+        "Aloe Vera": [
+        SeedName = "Aloe Vera Seed";
+		SeedRarity = "Legendary";
+		StockChance = 0;
+		StockAmount = {0, 0};
+		Price = 100000000000000000;
+		PurchaseID = 0;
+		DisplayInShop = false;
+		LayoutOrder = 350;
+		Asset = "https://create.roblox.com/store/asset/95215072760589";
+		FruitIcon = "https://create.roblox.com/store/asset/114480476983111";
+		GrowthTime = 1;
+        ],
+        
+        "Guanabana": [
+        SeedName = "Guanabana Seed";
+		SeedRarity = "Mythical";
+		StockChance = 0;
+		StockAmount = {0, 0};
+		Price = 100000000000000000;
+		PurchaseID = 0;
+		DisplayInShop = false;
+		LayoutOrder = 350;
+		Asset = "https://create.roblox.com/store/asset/133682848414531";
+		FruitIcon = "https://create.roblox.com/store/asset/71630132093184";
+		GrowthTime = 1;
+        ],
+        
+        "Noble Flower": [
+        SeedName = "Noble Flower";
+		SeedRarity = "Rare";
+		StockChance = 0;
+		StockAmount = {0, 0};
+		Price = 100000000000000000;
+		PurchaseID = 0;
+		DisplayInShop = false;
+		LayoutOrder = 900;
+		Asset = "https://create.roblox.com/store/asset/95187484637799";
+		FruitIcon = "";
+		GrowthTime = 1;
+        ],
+        
+        "Lemon": [
+        SeedName = "Lemon Seed";
+		SeedRarity = "Mythical";
+		StockChance = 0;
+		StockAmount = {0, 0};
+		Price = 100000000000000000;
+		PurchaseID = 0;
+		DisplayInShop = false;
+		LayoutOrder = 920;
+		Asset = "https://create.roblox.com/store/asset/116240364287848";
+		FruitIcon = "https://create.roblox.com/store/asset/116240364287848";
+		GrowthTime = 1;
+        ],
+        
+        "Cherry Blossom": [
+        SeedName = "Cherry Blossom Seed";
+		SeedRarity = "Divine";
+		StockChance = 0;
+		StockAmount = {0, 0};
+		Price = 100000000000000000;
+		PurchaseID = 0;
+		DisplayInShop = false;
+		LayoutOrder = 930;
+		Asset = "https://create.roblox.com/store/asset/116240364287848";
+		FruitIcon = "https://create.roblox.com/store/asset/116240364287848";
+		GrowthTime = 1;
+        ],
+        
+        "Ice Cream Bean": [
+        SeedName = "Ice Cream Bean Seed";
+		SeedRarity = "Mythical";
+		StockChance = 0;
+		StockAmount = {0, 0};
+		Price = 100000000000000000;
+		PurchaseID = 0;
+		DisplayInShop = false;
+		LayoutOrder = 940;
+		Asset = "https://create.roblox.com/store/asset/116240364287848";
+		FruitIcon = "https://create.roblox.com/store/asset/116240364287848";
+		GrowthTime = 1;
+        ],
+        
+        "Lime": [
+        SeedName = "Lime Seed";
+		SeedRarity = "Mythical";
+		StockChance = 0;
+		StockAmount = {0, 0};
+		Price = 100000000000000000;
+		PurchaseID = 0;
+		DisplayInShop = false;
+		LayoutOrder = 950;
+		Asset = "https://create.roblox.com/store/asset/116240364287848";
+		FruitIcon = "https://create.roblox.com/store/asset/116240364287848";
+		GrowthTime = 1;
+        ],
+        
+        "White Mulberry": [
+        SeedName = "White Mulberry Seed";
+		SeedRarity = "Legendary";
+		StockChance = 0;
+		StockAmount = {0, 0};
+		Price = 100000000000000000;
+		PurchaseID = 0;
+		DisplayInShop = false;
+		LayoutOrder = 350;
+		Asset = "https://create.roblox.com/store/asset/107705698836783";
+		FruitIcon = "https://create.roblox.com/store/asset/116240364287848";
+		GrowthTime = 1;
+        ],
+        
+        "Night Staff": [
+        SeedName = "Night Staff";
+		SeedRarity = "Legendary";
+		StockChance = 1;
+		StockAmount = {1, 1};
+		Price = 12000000;
+		PurchaseID = 3317153937;
+		DisplayInShop = true;
+		ShowOdds = true;
+		LayoutOrder = 1;
+		ItemType = "Gear";
+		Stack = 1;
+		Description = "Grants extra moonlit mutations per night cycle";
+		FallbackPrice = 199;
+        ],
+        
+        "Star Caller": [
+        SeedName = "Star Caller";
+		SeedRarity = "Legendary";
+		StockChance = 2;
+		StockAmount = {1, 1};
+		Price = 15000000;
+		PurchaseID = 3286452366;
+		DisplayInShop = true;
+		ShowOdds = true;
+		LayoutOrder = 10;
+		ItemType = "Gear";
+		Stack = 1;
+		Description = "Attracts shooting stars";
+		FallbackPrice = 219;
+        ],
+
+        "Mutation Spray Cloudtouched": [
+        SeedName = "Mutation Spray Cloudtouched";
+		SeedRarity = "Legendary";
+		StockChance = 2;
+		StockAmount = {1, 2};
+		Price = 50000000;
+		PurchaseID = 3317773711;
+		DisplayInShop = true;
+		ShowOdds = true;
+		LayoutOrder = 20;
+		ItemType = "Gear";
+		Stack = 3;
+		Description = "Fruits become blessed by clouds!";
+		FallbackPrice = 199;
+        ],
+
+        "Common Gnome Crate": [
+        SeedName = "Common Gnome Crate";
+		SeedRarity = "Common";
+		StockChance = 1;
+		StockAmount = {1, 1};
+		Price = 55500000;
+		PurchaseID = 3290108955;
+		DisplayInShop = true;
+		ShowOdds = true;
+		LayoutOrder = 10;
+		ItemType = "Crate";
+		Stack = 1;
+		Description = "";
+		FallbackPrice = 149;
+        ],
+
+        "Farmers Gnome Crate": [
+        SeedName = "Farmers Gnome Crate";
+		SeedRarity = "Rare";
+		StockChance = 1;
+		StockAmount = {1, 1};
+		Price = 88500000;
+		PurchaseID = 3290109302;
+		DisplayInShop = true;
+		ShowOdds = true;
+		LayoutOrder = 20;
+		ItemType = "Crate";
+		Stack = 1;
+		Description = "";
+		FallbackPrice = 179;
+        ],
+
+        "Classic Gnome Crate": [
+        SeedName = "Classic Gnome Crate";
+		SeedRarity = "Legendary";
+		StockChance = 2;
+		StockAmount = {1, 1};
+		Price = 113000000;
+		PurchaseID = 3290109380;
+		DisplayInShop = true;
+		ShowOdds = true;
+		LayoutOrder = 30;
+		ItemType = "Crate";
+		Stack = 1;
+		Description = "";
+		FallbackPrice = 199;
+        ],
+
+        "Iconic Gnome Crate": [
+        SeedName = "Iconic Gnome Crate";
+		SeedRarity = "Mythical";
+		StockChance = 4;
+		StockAmount = {1, 1};
+		Price = 155000000;
+		PurchaseID = 3317148476;
+		DisplayInShop = true;
+		ShowOdds = true;
+		LayoutOrder = 40;
+		ItemType = "Crate";
+		Stack = 1;
+		Description = "";
+		FallbackPrice = 199;
+        ],
+
+        "Mutation Spray Chilled": [
+        Type = "Recipe";
+        Inputs = "{ { ItemName = 'Cleaning Spray' }, { ItemName = 'Godly Sprinkler' } }";
+        Outputs = "{ { ItemName = 'Mutation Spray Chilled', Quantity = 1 } }";
+        TimeToCraft = 300;
+        RobuxPrice = 189;
+        Machine = "Gear Event Workbench";
+        Cost = "500,000 Sheckles";
+        ],
+
+        "Peace Lily": [
+        Type = "Recipe";
+        Inputs = "{ { ItemName = 'Rafflesia' }, { ItemName = 'Cauliflower' } }";
+        Outputs = "{ { ItemName = 'Peace Lily', Quantity = 1 } }";
+        TimeToCraft = 600;
+        RobuxPrice = 129;
+        Machine = "Seed Event Workbench";
+        Cost = "3 SummerCoins";
+        ],
+
+        "Guanabana": [
+        Type = "Recipe";
+        Inputs = "{ { ItemName = 'Aloe Vera' }, { ItemName = 'Banana' } }";
+        Outputs = "{ { ItemName = 'Guanabana', Quantity = 1 } }";
+        TimeToCraft = 600;
+        RobuxPrice = 779;
+        Machine = "Seed Event Workbench";
+        Cost = "30 SummerCoins";
+        ],
+
+        "Summer Seed Pack": [
+        SeedName = "Summer Seed Pack";
+		SeedRarity = "Rare";
+		StockChance = 1;
+		StockAmount = {1, 2};
+		Price = 3;
+		PurchaseID = 3317016327;
+		SpecialCurrencyType = "SummerCoins";
+		DisplayInShop = true;
+		ShowOdds = true;
+		LayoutOrder = 1;
+		ItemType = "Seed Pack";
+		Stack = 1;
+		Description = "";
+		FallbackPrice = 199;
+        ],
+
+        "Mutation Spray Burnt": [
+        SeedName = "Mutation Spray Burnt";
+		SeedRarity = "Legendary";
+		StockChance = 3;
+		StockAmount = {1, 3};
+		Price = 3;
+		PurchaseID = 3317773094;
+		SpecialCurrencyType = "SummerCoins";
+		DisplayInShop = true;
+		LayoutOrder = 8;
+		ItemType = "Gear";
+		Stack = 1;
+		Description = "";
+		FallbackPrice = 189;
+        ],
+
+        "Oasis Crate": [
+        SeedName = "Oasis Crate";
+		SeedRarity = "Legendary";
+		StockChance = 6;
+		StockAmount = {1, 1};
+		Price = 5;
+		PurchaseID = 3317017128;
+		SpecialCurrencyType = "SummerCoins";
+		DisplayInShop = true;
+		ShowOdds = true;
+		LayoutOrder = 10;
+		ItemType = "Crate";
+		Stack = 1;
+		Description = "";
+		FallbackPrice = 179;
+        ],
+
+        "Oasis Egg": [
+        SeedName = "Oasis Egg";
+		SeedRarity = "Mythical";
+		StockChance = 8;
+		StockAmount = {1, 1};
+		Price = 10;
+		PurchaseID = 3317017036;
+		SpecialCurrencyType = "SummerCoins";
+		DisplayInShop = true;
+		ShowOdds = true;
+		LayoutOrder = 20;
+		ItemType = "Egg";
+		Stack = 1;
+		Description = "";
+		FallbackPrice = 149;
+        ],
+
+        "Hamster": [
+        SeedName = "Hamster";
+		SeedRarity = "Mythical";
+		StockChance = 15;
+		StockAmount = {1, 1};
+		Price = 15;
+		PurchaseID = 3317176760;
+		SpecialCurrencyType = "SummerCoins";
+		DisplayInShop = true;
+		ShowOdds = true;
+		LayoutOrder = 30;
+		ItemType = "Pet";
+		Stack = 1;
+		Description = "Hamster Wheel: Occasionally runs in a hamster wheel and grants increased crafting speed for a duration";
+		FallbackPrice = 449;
+        ],
+
+        "Magnifying Glass": [
+        GearName = "Magnifying Glass";
+		GearRarity = "Mythical";
+		StockChance = 3;
+		StockAmount = {1, 1};
+		Price = 10000000;
+		PurchaseID = 3316261725;
+		GiftPurchaseID = 3317955209;
+		Stack = 10;
+		DisplayInShop = true;
+		LayoutOrder = 80;
+		Asset = "https://create.roblox.com/store/asset/107914377975445";
+		GearDescription = Chalk.color(Color3.fromRGB(85, 255, 255))("Inspect plants to reveal the value, without collecting them!");
+		FallbackPrice = 99;
+        ],
+
+        "Oasis Egg": [
+        Color = Color3.fromRGB(255, 205, 119);
+		HatchTime = 15000;
+		Icon = "https://create.roblox.com/store/asset/76221946588708";
+        ],
+
+        "Meerkat": [
+        ItemOdd = 45;
+		WeightRange = 0.8, 2;
+		HugeChance = 0.001;
+        Description = "Lookout: Occasionally goes to another pet and does a lookout for it. That pet advances cooldown. Has a chance chance to do it again after each lookout.";
+		Actions = {};
+		Model = var6:FindFirstChild("Meerkat");
+		DefaultHunger = 22000;
+		WeldOffset = CFrame.Angles(0, 0, 0);
+		ToolWeldOffset = true;
+		ModelScalePerLevel = 0.005;
+		Icon = "https://create.roblox.com/store/asset/111346621796974";
+		HungerFruitMultipliers = {};
+		MovementType = "Grounded";
+		MovementSpeed = 9;
+		SellPrice = 1100000;
+		Rarity = "Legendary";
+		Passives = {"Lookout"};
+		YHeightScaler = 1;
+		Animations = {
+			Idle = PetAnimations.Meerkat.Idle;
+			Walk = PetAnimations.Meerkat.Walk;
+			Stand = PetAnimations.Meerkat.Stand;
+		};
+        ],
+        
+        "Sand Snake": [
+        ItemOdd = 34.5;
+		WeightRange = 0.8, 2;
+		HugeChance = 0.001;
+        Description = "Coiled Commerce: Buying from the seed/gear shop has a small chance to duplicate the bought item!";
+		Actions = {};
+		Model = var6:FindFirstChild("Sand Snake");
+		DefaultHunger = 28000;
+		WeldOffset = CFrame.Angles(0, 0, 0);
+		ToolWeldOffset = true;
+		ModelScalePerLevel = 0.005;
+		Icon = "https://create.roblox.com/store/asset/110203289397199";
+		HungerFruitMultipliers = {};
+		MovementType = "Grounded";
+		MovementSpeed = 6;
+		SellPrice = 1100000;
+		Rarity = "Legendary";
+		Passives = {"Coiled Commerce"};
+		YHeightScaler = 0.1;
+		Animations = {
+			Idle = PetAnimations.Snake.Idle;
+			Walk = PetAnimations.Snake.Walk;
+		};
+        ],
+
+        "Axolotl": [
+        ItemOdd = 15;
+		WeightRange = 0.8, 2;
+		HugeChance = 0.001;
+        Description = "Summer Regeneration: Summer type fruits have a chance to stay after collecting!";
+		Actions = {};
+		Model = var6:FindFirstChild("Axolotl");
+		DefaultHunger = 22000;
+		WeldOffset = CFrame.Angles(0, 0, 0);
+		ToolWeldOffset = true;
+		ModelScalePerLevel = 0.005;
+		Icon = "https://create.roblox.com/store/asset/115096754077953";
+		HungerFruitMultipliers = {};
+		MovementType = "Grounded";
+		MovementSpeed = 5;
+		SellPrice = 3300000;
+		Rarity = "Mythical";
+		Passives = {"Summer Regeneration"};
+		YHeightScaler = 0.5;
+		Animations = {
+			Idle = PetAnimations.Axolotl.Idle;
+			Walk = PetAnimations.Axolotl.Walk;
+		};
+        ],
+        
+        "Hyacinth Macaw": [
+        ItemOdd = 5;
+		WeightRange = 0.8, 2;
+		HugeChance = 0.001;
+        Description = "Wiltproof Bird: Occasionally has a chance to mutate nearby fruits Wiltproof";
+		Actions = {};
+		Model = var6:FindFirstChild("Hyacinth Macaw");
+		DefaultHunger = 12000;
+		WeldOffset = CFrame.Angles(0, (-math.pi/2), 0);
+		ToolWeldOffset = true;
+		ModelScalePerLevel = 0.005;
+		Icon = "https://create.roblox.com/store/asset/118586950763516";
+		HungerFruitMultipliers = {};
+		MovementType = "Grounded";
+		MovementSpeed = 7;
+		SellPrice = 4400000;
+		Rarity = "Mythical";
+		Passives = {"Cloudtouched Bird", "Movement Variation"};
+		YHeightScaler = 0.75;
+		Animations = {
+			Idle = PetAnimations.ScarletMacaw.Idle;
+			Walk = PetAnimations.ScarletMacaw.Walk;
+			Fly = PetAnimations.ScarletMacaw.Fly;
+			FlyUp = PetAnimations.ScarletMacaw.FlyUp;
+			FlyDown = PetAnimations.ScarletMacaw.FlyDown;
+		};
+        ],
+        
+        "Fennec Fox": [
+        ItemOdd = 0.5;
+		WeightRange = 0.8, 2;
+		HugeChance = 0.001;
+        Description = "Sly: Occasionally goes to another player's random fruit, has a chance to copy 1 mutation from it and applies it to a random fruit you own.";
+		Actions = {};
+		Model = var6:FindFirstChild("Fennec Fox");
+		DefaultHunger = 35000;
+		WeldOffset = CFrame.Angles(0, 0, 0);
+		ToolWeldOffset = true;
+		ModelScalePerLevel = 0.005;
+		Icon = "https://create.roblox.com/store/asset/106568248173155";
+		HungerFruitMultipliers = {};
+		MovementType = "Grounded";
+		MovementSpeed = 16;
+		SellPrice = 7500000;
+		Rarity = "Divine";
+		Passives = {"Sly"};
+		YHeightScaler = 1.1;
+		Animations = {
+			Idle = PetAnimations.FennecFox.Idle;
+			Walk = PetAnimations.FennecFox.Walk;
+			Steal = PetAnimations.FennecFox.Steal;
+		};
+        ],
+        
+        "Firefly": [
+        Description = "Lightning Bug: Occasionally strikes a random nearby fruit, with a small chance of turning it Shocked";
+		Actions = {};
+		Model = var6:FindFirstChild("Firefly");
+		DefaultHunger = 25000;
+		WeldOffset = CFrame.Angles(0, (-math.pi/2), (math.pi/2));
+		ModelScalePerLevel = 0.005;
+		Icon = "https://create.roblox.com/store/asset/131310748087635";
+		HungerFruitMultipliers = {};
+		MovementType = "Flight";
+		MovementSpeed = 10;
+		SellPrice = 3300000;
+		Rarity = "Mythical";
+		Passives = {"Lightning Bug"};
+		YHeightScaler = 0.5;
+		Animations = {
+			Idle = PetAnimations.Firefly.Idle;
+			Walk = PetAnimations.Firefly.Walk;
+		};
+        ],
+
+        "Red Dragon": [
+        Description = "Scorched Soil: Occasionally sets a random nearby fruit ablaze, turning it into Burnt";
+		Actions = {};
+		Model = var6:FindFirstChild("Red Dragon");
+		DefaultHunger = 10000;
+		WeldOffset = CFrame.Angles(0, (-math.pi/2), 0);
+		ToolWeldOffset = true;
+		ModelScalePerLevel = 0.004;
+		Icon = "https://create.roblox.com/store/asset/140223014467344";
+		HungerFruitMultipliers = {};
+		MovementType = "Flight";
+		MovementSpeed = 10;
+		SellPrice = 100000;
+		Rarity = "Common";
+		Passives = {"Scorched Soil"};
+		YHeightScaler = 0.2;
+		Animations = {};
+        ],
+
+        "Golden Bee": [
+        Description = "Pollinator: Occasionally pollinates fruit & Golden Harvest: Harvested fruit has a chance to turn to gold";
+		Actions = {};
+		Model = var6:FindFirstChild("Bee");
+		Variant = "Golden Bee";
+		DefaultHunger = 25000;
+		WeldOffset = CFrame.Angles(0, (math.pi/2), (-math.pi));
+		ToolWeldOffset = true;
+		ModelScalePerLevel = 0.005;
+		Icon = "https://create.roblox.com/store/asset/125658111834879";
+		HungerFruitMultipliers = {};
+		MovementType = "Flight";
+		MovementSpeed = 9;
+		SellPrice = 1000000;
+		Rarity = "Mythical";
+		Passives = {"Pollinator", "Golden Harvest"};
+		YHeightScaler = 0.03;
+		Animations = {
+			Idle = PetAnimations.Bee.Idle;
+			Walk = PetAnimations.Bee.Walk;
+			Pollinate = PetAnimations.Bee.Pollinate;
+		};
+        ],
+
+        "cloudtouched": [
+        Multiplier = 5
+        ],
+
+        "fried": [
+        Multiplier = 8
+        ],
+
+        "aurora": [
+        Multiplier = 90
+        ],
+
+        "wiltproof": [
+        Multiplier = 4
+        ],
+
+        "drenched": [
+        Multiplier = 5
+        ],
+    `;
